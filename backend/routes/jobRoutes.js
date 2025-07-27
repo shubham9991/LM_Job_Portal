8416ba64fe83a2cbd3197e478871cf9565aa8cd4
shubham9991 <shubham321098@gmail.com>
2025-07-27 19:35:22 +0530

    Rahul code push

--- backend/routes/jobRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getJobDetailsPublic } = require('../controllers/jobController');
-
-const router = express.Router();
-
-// Public job detail endpoint
-router.get('/:id', getJobDetailsPublic);
-
-module.exports = router;
